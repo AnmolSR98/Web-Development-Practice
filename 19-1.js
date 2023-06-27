216c98f2714d144cafd52cf8d1dea3d6a5410073
AnmolSR98 <137732353+AnmolSR98@users.noreply.github.com>
2023-06-27 17:48:38 -0600

    Add files via upload

---
+++ 19-1.js
@@ -0,0 +1,12 @@
+var friends = ["Akash", "Adam", "Jashanpreet", "Stephanie"];
+
+alert (friends[2]); //Should alert to Jashanpreet
+
+var name = "Anmol";
+
+if (name === "Jennifer") { 	alert("That's my name too!");	} // equal to and same data type
+
+var myVariable = 6;
+
+if (myVariable > 5) {	alert("Upper");	}
+else {	alert("Lower");	}
